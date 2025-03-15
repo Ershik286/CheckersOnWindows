ed9d365c1969d58a229bc18ca6a1c0a76ec614a2
Никита <nikitaershov64@gmail.com>
2025-03-15 19:54:05 +0300

    Update Checkers.h

--- Checkers.h
+++ Checkers.h
@@ -1,5 +1,18 @@
 #pragma once
-class Checkers
-{
+class Checkers{
+public:
+	int color = 0;
+	int posY;
+	int posX;
+	bool Plot = false;
+	bool Checker = false;
+	bool Lady = false;
+	int value;
+	void Move(Checkers NewPlot, Checkers Board[8][8], int newPlotLine, int newPlotColomn);
+private:
+	int CheckMove(Checkers NewPlot, Checkers Board[8][8]);
+	int CheckChop(Checkers Board[8][8], int allyColor);
+	int CheckDiagonal(Checkers Board[8][8], int y, int x, int color);
+	int CheckDiagonalChop(Checkers Board[8][8], int y, int x, int color, int newY, int newX);
 };
 
